4b45f1728f85f7a9b01276476d840a766329a7ca
Olia Kashchuk <144221216+oliako@users.noreply.github.com>
2026-03-26 09:41:55 +0000

    update README with insights

--- analysis.sql
+++ analysis.sql
@@ -18,3 +18,16 @@ FROM events
 GROUP BY event_date
 ORDER BY event_date;
 
+-- 5. Funnel
+SELECT
+    COUNT(DISTINCT CASE WHEN event_name = 'open_app' THEN user_id END) AS open_app_users,
+    COUNT(DISTINCT CASE WHEN event_name = 'start_lesson' THEN user_id END) AS start_users,
+    COUNT(DISTINCT CASE WHEN event_name = 'finish_lesson' THEN user_id END) AS finish_users
+FROM events;
+
+--6. 
+SELECT 
+    COUNT(DISTINCT s.user_id) * 1.0 / COUNT(DISTINCT u.user_id) AS conversion_rate
+FROM users u
+LEFT JOIN subscriptions s
+ON u.user_id = s.user_id;
